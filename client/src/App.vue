8692a8fb50232ee74a52bd843174f8e9676901a3
Julia Pacuła <paculajulia@gmail.com>
2021-10-30 23:13:25 +0200

    MJ-36 added error handling and fixed minor UI issues.

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script lang="ts">
+import { State } from '@/store/state';
 import { UserActions } from '@/store/user-module/actions';
 import Vue from 'vue';
 
@@ -11,6 +12,14 @@ export default Vue.extend({
   async beforeCreate() {
     await this.$store.dispatch(UserActions.GetConfiguration);
     await this.$store.dispatch(UserActions.Get);
+  },
+  created() {
+    this.$store.watch(
+      (s: State) => s.uiState.errorMessageKeys,
+      (messageKeys: string[]) => {
+        this.$toasted.error(this.$t(messageKeys[0]));
+      },
+    );
   },
 });
 </script>
