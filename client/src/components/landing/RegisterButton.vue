8ed8150c9a0d0ef31d0156c699de8d25d664abea
AniaPazniewska <ania105449@gmail.com>
2021-11-04 21:07:09 +0100

    MJ-58 Advanced landing page

---
+++ client/src/components/landing/RegisterButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button
+      class="btn btn-primary"
+      @click="register()">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'RegisterButton',
+  methods: {
+    register(): void {
+      // TODO: Add registration path retrival
+    },
+  },
+});
+</script>
+
+<style
+  lang="scss"
+  scoped>
+
+</style>
